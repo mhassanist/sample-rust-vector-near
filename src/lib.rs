c9f4c0ffcc156ca4435f65b2941384cb4e5f3a38
mohammed saudi <m.hassan@ogr.iu.edu.tr>
2022-03-15 15:03:43 +0300

    not working vector import

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,20 @@
-//نحتاج استيراد التالي حتى يمكن لبيئة العمل ترجمة السطور التالية و تحويلها لكود يعمل على البلوك تشين
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, near_bindgen};
+use near_sdk::collections::Vector;
+use near_sdk::near_bindgen;
+
+struct Writing {
+  pub text: String,
+  pub sender: String,
+  pub receiver: String,
+}
 
 near_sdk::setup_alloc!();
 
 #[near_bindgen]
-#[derive(Default, BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize)]
 pub struct Contract {
-  message: String,
+  pub writing_list: Vec<Writing>, //This line gives err
 }
-//#[near_bindgen]
-//ماكرو حتى يمكن لبيئة العمل ترجمة الكود التالي الى كود يعمل على البلوك تشين
+
 #[near_bindgen]
-impl Contract {
-  //دالة لتغيير قيمة المتغير
-  //message
-  pub fn set_message(&mut self, msg: String) -> String {
-    self.message = msg;
-    return "updated".to_string();
-  }
-
-  //دالة لإرجاع قيمة المتغير
-  //message
-  pub fn get_message(&self) -> String {
-    return self.message.to_string();
-  }
-
-  //دالة لفحص رصيد ههذا الحساب الموجود عليه الكود
-  //ملحوظة: تحتاج
-  //call
-  // و ليس view
-  pub fn get_balance() -> u128 {
-    //env
-    //مثل
-    //Context
-    //فى الأسمبلي سكريبت
-    //و هو وسيلتنا للوصول الى معلومات من البلوك تشين نفسه
-    //مثل أرصدة الحسابات وقيم الأموال المرسلة مع الاستعدعاءات و خلافه
-    return env::account_balance();
-  }
-}
+impl Contract {}
