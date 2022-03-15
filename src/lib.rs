af12ae2822bde1725eb71b74ce82a234a0e4de29
mohammed saudi <m.hassan@ogr.iu.edu.tr>
2022-03-15 16:45:06 +0300

    built but has errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,14 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::Vector;
 use near_sdk::near_bindgen;
+near_sdk::setup_alloc!();
 
-#[near_bindgen]
+#[derive(Default, BorshDeserialize, BorshSerialize)]
 pub struct Writing {
   pub text: String,
   pub sender: String,
   pub receiver: String,
 }
-
-near_sdk::setup_alloc!();
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
@@ -17,5 +16,26 @@ pub struct Contract {
   pub writing_list: Vector<Writing>, //This line gives err
 }
 
+impl Default for Contract {
+  fn default() -> Self {
+    return Default::default();
+  }
+}
+
 #[near_bindgen]
-impl Contract {}
+impl Contract {
+  pub fn add(&mut self) -> String {
+    let w = Writing {
+      text: "val".to_string(),
+      sender: "val".to_string(),
+      receiver: "val".to_string(),
+    };
+
+    let _ = &self.writing_list.push(&w);
+
+    return "Added".to_string();
+  }
+  pub fn get_writings(&self) -> u64 {
+    return self.writing_list.len();
+  }
+}
